79c6a6c1237be1fbc0d56622dca0a602a10fabd0
Gonzalo Almada <galmada@gmail.com>
2020-09-15 19:24:45 -0300

    Updated shop data with new data and cart utils to group items in cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,16 +1,23 @@
 import CartActionTypes from './cart.types'
+import { addItemToCart } from './cart.utils'
 
 const INITIAL_STATE = {
-    hidden : true
+    hidden : true,
+    cartItems: []
 };
 
-const CartReducer = (state = INITIAL_STATE, action) => {
+const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type){
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
                 hidden: !state.hidden
             }
+            case CartActionTypes.ADD_ITEM:
+                return{
+                    ...state,
+                    cartItems: addItemToCart(state.cartItems, action.payload)
+                }
         default:
             return state;
     }
@@ -18,4 +25,4 @@ const CartReducer = (state = INITIAL_STATE, action) => {
 }
 
 
-export default CartReducer;
+export default cartReducer;
